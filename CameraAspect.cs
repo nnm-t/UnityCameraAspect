caa95e7e604d7085f7a58ab264c5e4fbd73f1453
nnm_t <noname@kamisawa.net>
2017-07-11 23:43:06 +0900

    C#標準コード規約に修正

--- CameraAspect.cs
+++ CameraAspect.cs
@@ -3,34 +3,35 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-
-    public float x_aspect = 9.0f;
-    public float y_aspect = 16.0f;
+    [SerializeField]
+    private float aspectX = 9.0f;
+    [SerializeField]
+    private float aspectY = 16.0f;
 
     void Awake()
     {
         Camera camera = GetComponent<Camera>();
-        Rect rect = calcAspect(x_aspect, y_aspect);
+        Rect rect = CalcAspectRatio(aspectX, aspectY);
         camera.rect = rect;
     }
 
-    private Rect calcAspect(float width, float height)
+    private Rect CalcAspectRatio(float width, float height)
     {
-        float target_aspect = width / height;
-        float window_aspect = (float)Screen.width / (float)Screen.height;
-        float scale_height = window_aspect / target_aspect;
-        Rect rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+        float targetAspect = width / height;
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+        float heightScale = windowAspect / targetAspect;
+        var rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
 
-        if (1.0f > scale_height)
+        if (1.0f > heightScale)
         {
             rect.x = 0;
-            rect.y = (1.0f - scale_height) / 2.0f;
+            rect.y = (1.0f - heightScale) / 2.0f;
             rect.width = 1.0f;
-            rect.height = scale_height;
+            rect.height = heightScale;
         }
         else
         {
-            float scale_width = 1.0f / scale_height;
+            float scale_width = 1.0f / heightScale;
             rect.x = (1.0f - scale_width) / 2.0f;
             rect.y = 0.0f;
             rect.width = scale_width;
